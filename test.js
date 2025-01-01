4c44caa5bf035373ce18524b5c9f79e3f1987051
hansjovis <hc.braun@protonmail.com>
2025-01-01 14:12:40 +0100

    Updated test file to show HTML in console.

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import parse from './dist/sharkdown.js';
+import toHTML from './dist/html/toHTML.js';
 
 const text = await readFile('test.md', 'utf8');
 
 const document = parse(text);
 
+console.log('Document:');
 console.log(JSON.stringify(document, null, 2));
+
+const html = toHTML(document);
+console.log('HTML:');
+console.log(html);
+
+await writeFile('test.html', html);
